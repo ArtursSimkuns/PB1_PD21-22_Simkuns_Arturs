67cb23b00396c20a586fcc7f1f44c7a4aded0e27
Artūrs Šimkūns <arturs.simkuns@gmail.com>
2026-08-13 21:04:06 +0300

    fix: docker konfigurācija

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from utils import get_message
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return {"message": get_message()}
 
+
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=5000)
